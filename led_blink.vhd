b122f25dc45f02444b83ab5d1df6e563ab03f7cb
Anders Barzdukas abarzdukas@umail.ucsb.edu <abarzdukas@higgs.physics.ucsb.edu>
2023-09-27 12:30:34 -0700

    Added ILA

--- led_blink.vhd
+++ led_blink.vhd
@@ -14,6 +14,16 @@ end tutorial_led_blink;
  
 architecture rtl of tutorial_led_blink is
  
+  --ILA Declaration
+  component ila_0 is
+  port(
+    clk : in std_logic;
+    probe0 : in std_logic_vector(0 downto 0); --Note if your port widths are wrong it will not tell you
+    probe1 : in std_logic_vector(0 downto 0);
+    probe2 : in std_logic_vector(63 downto 0)
+   );
+  end component;
+  
   -- Constants to create the frequencies needed:
   -- Formula is: (25 MHz / 100 Hz * 50% duty cycle)
   -- So for 100 Hz: 25,000,000 / 100 * 0.5 = 125,000
@@ -22,13 +32,27 @@ architecture rtl of tutorial_led_blink is
  
   -- These signals will be the counters:
   signal r_CNT_1HZ   : natural range 0 to c_CNT_1HZ;
-   
+  signal r_CNT_stdvec : std_logic_vector(63 downto 0);
   -- These signals will toggle at the frequencies needed:
   signal r_TOGGLE_1HZ   : std_logic := '0';
  
    signal i_clock_buf :std_logic := '0';
    signal iclock : std_logic := '0';
+   
+   
 begin
+
+
+ r_CNT_stdvec <= std_logic_vector(to_unsigned(r_CNT_1HZ, r_CNT_stdvec'length));
+ 
+    ila_0_i : ila_0
+  port map(
+    clk => iclock,
+    probe0(0) => iclock,
+    probe1(0) => r_Toggle_1HZ,
+    probe2(63 to 0) => R_CNT_stdvec
+
+  );
  
   u_ibufgds_iclock : IBUFGDS
     port map (
